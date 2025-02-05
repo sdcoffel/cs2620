b9d2b63dfe753597d1dfe067d686eacadcf296af
Savanna Coffel <sdcoffel@college.harvard.edu>
2025-02-05 10:46:40 -0500

    first attempt at connection

--- design exercise 1/client.py
+++ design exercise 1/client.py
@@ -1,1 +1,82 @@
+import socket
+import threading
 
+def receive_messages(sock):
+    """ This function is in charge of recieving messages that have been forwarded from the server. 
+
+    Args: The socket that the client is currently connected to the server with. 
+
+    Returns: Will return with an exception if the connection between the server and the client goes down. Else will continue until either the client or server
+    terminates the connection.
+
+    """
+    try:
+        while True:
+            message = sock.recv(1024).decode('utf-8')
+            if message:
+                print("\rReceived: " + message + "\nYou: ", end="")
+            else:
+                print("\nServer closed the connection.")
+                break
+    except Exception as e:
+        print(f"Error receiving data: {e}")
+    finally:
+        try:
+            sock.close()
+            print("Socket has been closed.")
+        except Exception as e:
+            print(f"Failed to close the socket properly: {e}")
+
+def send_messages(sock):
+    """Sends messages along the socket to the server. If an empty message is typed, the user has the power to 
+    terminate the connection when prompted. Different error handling mechanisms are at the bottom of the function. 
+
+    Args: 
+        sock (socket.socket): Socket the client is currently connected with. 
+
+    Continues until the connection is terminated, handles exceptions if the message cannot be sent.
+    
+    """
+    try:
+        while True:
+            message = input("You: ")
+            if not message:
+                confirm = input("No message entered. Would you like to end the connection? (yes/no) ").strip().lower()
+                if confirm == 'yes':
+                    print("Ending connection...")
+                    break
+                elif confirm == 'no':
+                    continue
+                else:
+                    print("Please type 'yes' or 'no'.")
+                    continue
+
+            sock.send(message.encode('utf-8'))
+
+    except socket.error as e:
+        print(f"Error sending message: {e}")
+    except KeyboardInterrupt:
+        print("You have exited the chat.")
+    except Exception as e:
+        print(f"Some unexpected error {e} has occurred: Please contact system administrators Savanna and Ian")
+    finally:
+        try:
+            sock.close()
+            print("Socket closed.")
+        except Exception as e:
+            print(f"Failed to close the socket properly: {e}")
+
+
+def start_client():
+    """Responsible for booting up the client and establishing the first connection to the server. 
+
+    """
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 12345)) #we should not hardcode this, i have to change this every time and waldo will fail us 
+
+    threading.Thread(target=receive_messages, args=(client_socket,)).start()
+    send_messages(client_socket)
+
+if __name__ == "__main__":
+    #currently, i broadcast messages to everybody except the source client. i want to make this so that i can specify by username who to send a message to. that requires knowing usernames
+    start_client()
--- design exercise 1/server.py
+++ design exercise 1/server.py
@@ -1,1 +1,72 @@
+import socket
+import threading
 
+clients = []  #list of all clients currently online
+
+def broadcast_message(message, sender_socket): 
+    """This function broadcasts a client's message to everyone else by forwarding via the server. We need to tweak this by username eventually.
+
+    Args: 
+        message: The incoming message from the client. 
+        sender_socket: The socket that belongs to the sender.
+
+        If the server cannot get a message through, throw an error but do not terminate the connection with the client
+
+    """
+    for client in clients:
+        if client != sender_socket:
+            try:
+                client.send(message)
+            except Exception as e: 
+                print(f"Error sending message to {client.getpeername()}: {e}")
+                continue
+
+
+def client_handler(connection, address):
+    """Establishes a connection with the client. 
+
+    Args: 
+        connection (socket.socket()): socket associated with the client
+        address: IP address and port number of the client
+
+    """
+    print(f"Connected with {address}")
+    clients.append(connection)  
+    
+    while True:
+        message = connection.recv(1024)
+        if not message:
+            break
+        broadcast_message(message, connection) #all clients except the sender recieve a broadcast - an area to fix 
+
+
+def start_server():
+    """Responsible for booting up the server. 
+    
+    This will run until the server encounters an exception or is manually shut off.
+
+    """
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  #this line guarantees that we can reuse the same port over and over again without having to change the number
+        server_socket.bind(('localhost', 12345))
+        server_socket.listen()
+        print("Server is listening...")
+
+        while True:
+            try:
+                client_socket, addr = server_socket.accept()
+                thread = threading.Thread(target=client_handler, args=(client_socket, addr)) #in order to have multiple clients, i might model each of them as a thread. results pending on if this is a smart move or not
+                thread.start()
+            except Exception as e: 
+                print(f"Fatal error {e} with server")
+    
+    finally: #close the socket gracefully
+        try: 
+            server_socket.close()
+        except Exception as e: 
+            print(f"Failed to close server socket properly! : {e}")
+         
+
+if __name__ == "__main__":
+    start_server()
