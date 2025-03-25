b915809b67bd232a438e303f6b7bc916ca698291
Savanna Coffel <sdcoffel@college.harvard.edu>
2025-03-24 22:00:30 -0400

    test for communication hehe

--- design_exercise_3/zookeeper_manager.py
+++ design_exercise_3/zookeeper_manager.py
@@ -1,6 +1,6 @@
 from kazoo.client import KazooClient
 
-ZOOKEEPER_HOSTS = "127.0.0.1:2181"  #ZooKeeper host - ian needs to update this on his side so his servers can communicate w mine
+ZOOKEEPER_HOSTS = "10.250.84.166"  #ZooKeeper host - ian needs to update this on his side so his servers can communicate w mine
 
 class ZooKeeperManager:
     def __init__(self):
